0bf3d52efe9ec70d545b39286d5dd32211e58cc9
Lijie Sun <47992432+sunlijie-msu@users.noreply.github.com>
2024-06-21 22:52:41 -0400

    59Cu p,g p,a reaction rate

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Bayesian_DSL for TRIUMF EEC Experiment S2373
 Resonance_Strength for TRIUMF EEC Experiment S2373 Uncertainty Propagation
 
 Bayesian_Fit_Exp for FRIB PXCT 241Am Source Test
+
+Reaction_Rate_PXCT for PXCT 59Cu(p,a)56Ni Reaction Rate Calculation.
--- Reaction_Rate_PXCT.py
+++ Reaction_Rate_PXCT.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import matplotlib.lines as mlines
+import matplotlib.ticker as ticker
+import matplotlib.cm as cm
 from random import sample
 import scipy.stats as sps
 from scipy.stats import poisson
@@ -19,7 +21,17 @@ import seaborn as sns
 import pylab
 
 
-# Constants
+# plots a list of profiles in the same figure. Each profile corresponds to a simulation replica for the given instance.
+plt.rcParams['axes.linewidth'] = 2.0
+plt.rcParams['font.size'] = 32
+font_family_options = ['Times New Roman', 'Georgia', 'Cambria', 'Courier New', 'serif']
+plt.rcParams['font.family'] = font_family_options
+# plt.rcParams['mathtext.default'] = 'regular'
+plt.rcParams['mathtext.fontset'] = 'custom'
+plt.rcParams['mathtext.rm'] = 'Times New Roman'
+plt.rcParams['mathtext.it'] = 'Times New Roman:italic'
+
+# Inputs for 128 60Zn resonances from Brown or Rauscher
 Jr = [1, 5, 2, 3, 4, 1, 4, 4, 0, 3, 2, 2, 5, 3, 3, 1, 4, 1, 2, 2, 4, 5, 0, 2, 4, 3, 4, 3, 2, 0, 1, 4, 5, 4, 2, 5, 3, 4, 2, 3, 2, 5, 5, 3, 1, 2, 0, 4, 4, 2, 1, 5, 3, 0, 4, 3, 2, 3, 1, 2, 3, 5, 4, 2, 5, 2, 1, 0, 5, 4, 1, 4, 2, 2, 3, 2, 3, 5, 1, 2, 3, 5, 4, 2, 1, 5, 2, 0, 2, 3, 3, 4, 5, 4, 3, 1, 4, 2, 2, 3, 4, 5, 1, 4, 2, 1, 3, 4, 3, 0, 0, 3, 4, 2, 2, 3, 4, 3, 5, 1, 4, 2, 3, 2, 4, 1, 3, 3]  # 60Zn resonance spins from Brown
 
 Er = [0.012, 0.083, 0.121, 0.147, 0.155, 0.168, 0.223, 0.241, 0.245, 0.27, 0.392, 0.396, 0.405, 0.437, 0.452, 0.461, 0.488, 0.538, 0.54, 0.563, 0.603, 0.653, 0.689, 0.741, 0.811, 0.825, 0.845, 0.872, 0.884, 0.933, 0.943, 0.944, 0.95, 0.959, 0.967, 0.989, 1.001, 1.005, 1.006, 1.007, 1.023, 1.075, 1.081, 1.091, 1.092, 1.14, 1.148, 1.149, 1.164, 1.196, 1.2, 1.22, 1.246, 1.28, 1.285, 1.288, 1.289, 1.3, 1.315, 1.316, 1.324, 1.325, 1.353, 1.372, 1.388, 1.39, 1.405, 1.41, 1.45, 1.456, 1.481, 1.502, 1.504, 1.515, 1.521, 1.533, 1.537, 1.592, 1.599, 1.638, 1.639, 1.647, 1.657, 1.661, 1.668, 1.673, 1.674, 1.71, 1.717, 1.744, 1.753, 1.768, 1.794, 1.808, 1.81, 1.816, 1.822, 1.841, 1.862, 1.876, 1.885, 1.904, 1.906, 1.908, 1.931, 1.94, 1.955, 1.977, 1.989, 1.99, 2.01, 2.028, 2.03, 2.059, 2.074, 2.075, 2.092, 2.112, 2.114, 2.131, 2.133, 2.138, 2.142, 2.148, 2.156, 2.158, 2.163, 2.185] # 60Zn resonance energies in MeV from Brown
@@ -43,15 +55,16 @@ Reduced_Mass = Ap*AT/(Ap+AT)  # reduced mass
 # gaussian random factor with mean 0.3662 and standard deviation 0.8948
 # This is a random factor to account for the uncertainty in the statistical model calculated alpha width
 # np.random.seed(0) # Set seed0 for reproducibility
-Log_Random = np.random.normal(0.3662, 0.8948, len(Jr))
-Random = 10**Log_Random # Convert to linear scale
-print(Random)
+# Log_Random = np.random.normal(0.3662, 0.8948, len(Jr))
+# Random = 10**Log_Random # Convert to linear scale
+# print(Random)
 
 # Calculate OmegaGamma element-wise
-# OmegaGamma = [(2 * Jr[i] + 1) / (2 * JT + 1) / (2 * Jp + 1) * Gp[i] * Ga[i] / (Gp[i] + Ga[i] + Gg[i]) for i in range(len(Jr))]
-OmegaGamma = [(2 * Jr[i] + 1) / (2 * JT + 1) / (2 * Jp + 1) * Gp[i] * Ga[i] * Random[i] / (Gp[i] + Ga[i] * Random[i] + Gg[i]) for i in range(len(Jr))]
+OmegaGamma_pa = [(2 * Jr[i] + 1) / (2 * JT + 1) / (2 * Jp + 1) * Gp[i] * Ga[i] / (Gp[i] + Ga[i] + Gg[i]) for i in range(len(Jr))]
+OmegaGamma_pg = [(2 * Jr[i] + 1) / (2 * JT + 1) / (2 * Jp + 1) * Gp[i] * Gg[i] / (Gp[i] + Ga[i] + Gg[i]) for i in range(len(Jr))]
+# OmegaGamma_pa = [(2 * Jr[i] + 1) / (2 * JT + 1) / (2 * Jp + 1) * Gp[i] * Ga[i] * Random[i] / (Gp[i] + Ga[i] * Random[i] + Gg[i]) for i in range(len(Jr))]
 
-T9 = np.linspace(0.001, 2.0, 300)  # temperature in GK
+T9 = np.linspace(0.001, 2.0, 500)  # temperature in GK
 
 # Reaction rates calculation function
 def Calculate_Rate(T9, Er, OmegaGamma):
@@ -61,29 +74,145 @@ def Calculate_Rate(T9, Er, OmegaGamma):
             Rate[i, j] = 1.5394E11 * (Reduced_Mass * T9[i]) ** (-1.5) * OmegaGamma[j] * np.exp(-11.605 * Er[j] / T9[i])
     return Rate
 
-Rate = Calculate_Rate(T9, Er, OmegaGamma)
+Rate_pa = Calculate_Rate(T9, Er, OmegaGamma_pa)
+Rate_pg = Calculate_Rate(T9, Er, OmegaGamma_pg)
 
-# Calculate total reaction rate, avoiding division by zero
-epsilon = 1e-60  # Small value to avoid division by zero
-Total_Rate = np.sum(Rate, axis=1) + epsilon
+# Calculate total reaction rate
+epsilon = 1e-80  # Small value to avoid division by zero
+Total_Rate_pa = np.sum(Rate_pa, axis=1) + epsilon
+Total_Rate_pg = np.sum(Rate_pg, axis=1) + epsilon
 
 # Calculate contribution of each resonance to the total rate
-Contribution = Rate / Total_Rate[:, None]
+Contribution_pa = Rate_pa / Total_Rate_pa[:, None]
+Contribution_pg = Rate_pg / Total_Rate_pg[:, None]
 
-# Plot
-fig, ax = plt.subplots(figsize=(10, 6))
+
+# Plot for (p,a) reaction rate
+# Define specific colors for high contribution resonances
+specific_colors = {
+    0.396: 'darkviolet',
+    0.540: 'green',
+    0.563: 'darkorange',
+    0.603: 'maroon',
+    0.884: 'red',
+    0.933: 'goldenrod',
+    0.967: 'royalblue',
+    1.006: 'magenta',
+    1.196: 'lime',
+    # Add more specific colors as needed
+}
+
+fig, ax = plt.subplots(figsize=(11, 7))
+# Plot all black curves first
 for i in range(len(Er)):
-    if np.max(Contribution[:, i]) > 0.1:  # Only add to legend if contribution exceeds 20%
-        ax.plot(T9, Contribution[:, i], label=f'E$_r$ = {Er[i]:.3f} MeV')
-    else:
-        ax.plot(T9, Contribution[:, i])
+    energy = Er[i]
+    if energy not in specific_colors:
+        ax.plot(T9, Contribution_pa[:, i]*100, color='black', linewidth=1.5)
 
-ax.set_xlabel('Temperature (GK)', fontsize=20)
-ax.set_ylabel('Contribution to total reaction rate', fontsize=20)
+# Plot colored curves on top
+for i in range(len(Er)):
+    energy = Er[i]
+    if energy in specific_colors:
+        color = specific_colors[energy]
+        ax.plot(T9, Contribution_pa[:, i]*100, color=color, linewidth=3)
+
+# Add annotations with arrows for specific energies with adjusted positions
+annotations = {
+    0.396: {'xytext': (0.11, 92)},
+    0.540: {'xytext': (0.19, 62)},
+    0.563: {'xytext': (0.34, 54)},
+    0.603: {'xytext': (0.38, 43)},
+    0.884: {'xytext': (0.58, 48)},
+    0.933: {'xytext': (0.61, 38)},
+    0.967: {'xytext': (0.71, 29)},
+    1.006: {'xytext': (0.81, 21)},
+    1.196: {'xytext': (1.10, 16)},
+    # Add more annotations as needed with their positions
+}
+
+for energy, color in specific_colors.items():
+    i = Er.index(energy)
+    max_contribution = np.max(Contribution_pa[:, i]*100)
+    max_index = np.argmax(Contribution_pa[:, i])
+    annotation_info = annotations[energy]
+    ax.annotate(f'{1000*energy:.0f}', xy=(T9[max_index], max_contribution),
+                xytext=annotation_info['xytext'],
+                arrowprops=dict(facecolor=color, edgecolor=color, shrink=0.05, width=1, headwidth=6),
+                fontsize=30, color=color)
+
+ax.set_xlabel('Temperature (GK)', fontsize=34, labelpad=14)
+ax.set_ylabel('Contribution (%)', fontsize=34, labelpad=24)
 ax.set_xlim(0, 2)
-ax.set_ylim(0, 1)
-ax.tick_params(axis='both', which='major', labelsize=18)
-ax.legend(fontsize=13, loc='upper right')
+ax.set_ylim(0, 100)
+ax.tick_params(axis='both', which='major', direction='in', length=9, width=2, labelsize=34, pad=10)
+ax.set_xticks(np.arange(0, 2.1, 0.5))  # Set ticks at intervals of 0.5
+plt.text(1.9, 95, r'$^{59}$Cu$(p,\alpha)^{56}$Ni', fontsize=38, color='black', horizontalalignment='right', verticalalignment='top')
 
-plt.tight_layout()
-plt.show()
+fig.subplots_adjust(left=0.16, bottom=0.18, right=0.96, top=0.96)
+# Save figure with the corresponding random number index in D:\X\out
+# plt.show()
+plt.savefig(f'D:\\X\\out\\Fig_PXCT_59Cu_pa_Reaction_Rate_Contribution.eps', dpi=300)
+
+
+# Plot for (p,g) reaction rate
+# Define specific colors for high contribution resonances
+specific_colors = {
+    0.396: 'darkviolet',
+    0.461: 'darkorange',
+    0.540: 'green',
+    0.884: 'red',
+    0.967: 'royalblue',
+    1.200: 'lime',
+    # Add more specific colors as needed
+}
+
+fig, ax = plt.subplots(figsize=(11, 7))
+# Plot all black curves first
+for i in range(len(Er)):
+    energy = Er[i]
+    if energy not in specific_colors:
+        ax.plot(T9, Contribution_pg[:, i]*100, color='black', linewidth=2)
+
+# Plot colored curves on top
+for i in range(len(Er)):
+    energy = Er[i]
+    if energy in specific_colors:
+        color = specific_colors[energy]
+        ax.plot(T9, Contribution_pg[:, i]*100, color=color, linewidth=3)
+
+# Add annotations with arrows for specific energies with adjusted positions
+annotations = {
+    0.396: {'xytext': (0.09, 92)},
+    0.461: {'xytext': (0.165, 62)},
+    0.540: {'xytext': (0.34, 65)},
+    0.884: {'xytext': (0.55, 33)},
+    0.967: {'xytext': (0.75, 70)},
+    1.200: {'xytext': (1.70, 20)},
+    # Add more annotations as needed with their positions
+}
+
+for energy, color in specific_colors.items():
+    i = Er.index(energy)
+    max_contribution = np.max(Contribution_pg[:, i]*100)
+    max_index = np.argmax(Contribution_pg[:, i])
+    annotation_info = annotations[energy]
+    ax.annotate(f'{1000*energy:.0f}', xy=(T9[max_index], max_contribution),
+                xytext=annotation_info['xytext'],
+                arrowprops=dict(facecolor=color, edgecolor=color, shrink=0.05, width=1, headwidth=6),
+                fontsize=30, color=color)
+
+ax.set_xlabel('Temperature (GK)', fontsize=34, labelpad=14)
+ax.set_ylabel('Contribution (%)', fontsize=34, labelpad=24)
+ax.set_xlim(0, 2)
+ax.set_ylim(0, 100)
+ax.tick_params(axis='both', which='major', direction='in', length=9, width=2, labelsize=34, pad=10)
+ax.set_xticks(np.arange(0, 2.1, 0.5))  # Set ticks at intervals of 0.5
+plt.text(1.9, 95, r'$^{59}$Cu$(p,\gamma)^{60}$Zn', fontsize=38, color='black', horizontalalignment='right', verticalalignment='top')
+
+fig.subplots_adjust(left=0.16, bottom=0.18, right=0.96, top=0.96)
+# Save figure with the corresponding random number index in D:\X\out
+# plt.show()
+plt.savefig(f'D:\\X\\out\\Fig_PXCT_59Cu_pg_Reaction_Rate_Contribution.eps', dpi=300)
+    
+
+
