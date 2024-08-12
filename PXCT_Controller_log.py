0799fd09493d48fb13f047531d47f5a09e0fcf9f
Lijie Sun <47992432+sunlijie-msu@users.noreply.github.com>
2024-08-12 03:05:29 -0400

    PXCT temp monitor

--- PXCT_Controller_log.py
+++ PXCT_Controller_log.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from datetime import timedelta
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.ticker as mticker
@@ -6,7 +8,7 @@ import os
 
 
 # plots a list of profiles in the same figure. Each profile corresponds to a simulation replica for the given instance.
-plt.rcParams['axes.linewidth'] = 3.0
+plt.rcParams['axes.linewidth'] = 4.0
 plt.rcParams['font.size'] = 60
 font_family_options = ['Times New Roman', 'Georgia', 'Cambria', 'Courier New', 'serif']
 plt.rcParams['font.family'] = font_family_options
@@ -42,31 +44,32 @@ lege_data = convert_to_datetime(lege_data)
 # Function to plot and save figures
 def plot_data(title, ylabel, data_field, file_name):
     fig, ax = plt.subplots(figsize=(36, 13))
-    fig.subplots_adjust(left=0.10, bottom=0.20, right=0.98, top=0.91)
+    fig.subplots_adjust(left=0.10, bottom=0.21, right=0.96, top=0.98)
     
-    ax.scatter(north_data['Time'], north_data[data_field], color='red', s=6, label='North', alpha=0.9, linewidth=0)
-    ax.scatter(south_data['Time'], south_data[data_field], color='blue', s=6, label='South', alpha=0.9, linewidth=0)
-    ax.scatter(lege_data['Time'], lege_data[data_field], color='green', s=6, label='LEGe', alpha=0.9, linewidth=0)
+    ax.scatter(north_data['Time'], north_data[data_field], color='red', s=28, label='North', alpha=0.9, linewidth=0)
+    ax.scatter(south_data['Time'], south_data[data_field], color='#0033ff', s=28, label='South', alpha=0.9, linewidth=0)
+    ax.scatter(lege_data['Time'], lege_data[data_field], color='green', s=28, label='LEGe', alpha=0.9, linewidth=0)
     
-    ax.set_title(title, fontsize=60)
-    ax.set_xlabel('Time', fontsize=60, labelpad=40)
-    ax.set_ylabel(ylabel, fontsize=60, labelpad=40)
+    # ax.set_title(title, fontsize=60)
+    ax.set_xlabel('Time', fontsize=64, labelpad=40)
+    ax.set_ylabel(ylabel, fontsize=64, labelpad=40)
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
     ax.yaxis.set_major_formatter(mticker.StrMethodFormatter('{x:.0f}'))
-    ax.legend(fontsize=60, loc='best', markerscale=3)
-    ax.tick_params(axis='both', labelsize=60, length=9, width=2)
+    legend = ax.legend(fontsize=64, loc='best', markerscale=3, frameon=False, labelspacing=0.24, handletextpad=-0.3)
+    for text, color in zip(legend.get_texts(), ['red', '#0033ff', 'green']):
+        text.set_color(color)
+    
+    ax.tick_params(axis='x', labelsize=64, length=9, width=3)
+    ax.tick_params(axis='y', labelsize=64, length=9, width=3)
     plt.xticks(rotation=45)
     
     ax.grid(True)
-    
-    for spine in ax.spines.values():
-        spine.set_linewidth(2)
     
     # Save the figure to the specified directory
     full_path = os.path.join(save_directory, file_name)
     plt.savefig(full_path)
 
 # Plot and save figures
-plot_data('Cooler Power Over Time', 'Cooler power', 'Cooler power', 'cooler_power.png')
-plot_data('Coldtip Temperature Over Time', 'Coldtip temp', 'Coldtip temp', 'coldtip_temp.png')
-plot_data('Coldhead Temperature Over Time', 'Coldhead temp', 'Coldhead temp', 'coldhead_temp.png')
+plot_data('Cooler Power Over Time', 'Cooler power (W)', 'Cooler power (W)', 'Fig_PXCT_Cooler_Power.png')
+plot_data('Coldtip Temperature Over Time', 'Coldtip T (°C)', 'Coldtip T (°C)', 'Fig_PXCT_Coldtip_Temp.png')
+plot_data('Coldhead Temperature Over Time', 'Coldhead T (°C)', 'Coldhead T (°C)', 'Fig_PXCT_Coldhead_Temp.png')
---
+++ PXCT_iPA_log.py
@@ -0,0 +1,88 @@
+import pandas as pd
+from datetime import timedelta
+import matplotlib.pyplot as plt
+import os
+
+# Global settings for plots
+plt.rcParams['axes.linewidth'] = 4.0
+plt.rcParams['font.size'] = 60
+plt.rcParams['font.family'] = ['Times New Roman', 'Georgia', 'Cambria', 'Courier New', 'serif']
+plt.rcParams['mathtext.fontset'] = 'custom'
+plt.rcParams['mathtext.rm'] = 'Times New Roman'
+plt.rcParams['mathtext.it'] = 'Times New Roman:italic'
+plt.rcParams['legend.frameon'] = False
+
+# Function to parse timestamps in 'd:hh:mm:ss' format
+def parse_time(time_string):
+    if time_string == '':
+        return None
+    parts = list(map(int, time_string.split(':')))
+    return timedelta(days=parts[0], hours=parts[1], minutes=parts[2], seconds=parts[3])
+
+# Read the CSV files
+df_north = pd.read_csv(r'D:\X\out\Bayesian_VS\North_5593_iPAlog.csv', converters={'Time Stamp': parse_time})
+df_south = pd.read_csv(r'D:\X\out\Bayesian_VS\South_5596_iPAlog.csv', converters={'Time Stamp': parse_time})
+df_lege = pd.read_csv(r'D:\X\out\Bayesian_VS\LEGe_13725_iPAlog.csv', converters={'Time Stamp': parse_time})
+
+# Parameters to filter
+parameters = ["11:PRTD 1", "12:PRTD 2", "13:Ambient Temperature", "73:DC Detector Leakage Current", "16:Charge Loop DC Level"]
+
+# Custom y-axis labels for each plot
+custom_y_labels = [
+    "PRTD1 (°C)",
+    "PRTD2 (°C)",
+    "Ambient T (°C)",
+    r"$\mathit{I}_{\mathrm{leak}}$ (pA)",  # Updated LaTeX syntax
+    "DC Level (V)"
+]
+
+# Filter the data
+df_north_filtered = [df_north[df_north["Parameter Code"] == param] for param in parameters]
+df_south_filtered = [df_south[df_south["Parameter Code"] == param] for param in parameters]
+df_lege_filtered = [df_lege[df_lege["Parameter Code"] == param] for param in parameters]
+
+# Set global x-axis min and max
+xmin, xmax = -0.1, 12
+
+# Create a 5x1 grid of plots
+fig, axs = plt.subplots(5, 1, figsize=(32, 25), sharex=True)
+plt.tight_layout()
+plt.subplots_adjust(top=0.96, bottom=0.10, hspace=0.23, left=0.11, right=0.97)
+
+# Plot each parameter
+for i, param in enumerate(parameters):
+    axs[i].scatter(df_north_filtered[i]["Time Stamp"].dt.total_seconds() / (24 * 60 * 60), df_north_filtered[i]["Parameter Value"], color='red', alpha=0.9, label='North', s=40, linewidth=0)
+    axs[i].scatter(df_south_filtered[i]["Time Stamp"].dt.total_seconds() / (24 * 60 * 60), df_south_filtered[i]["Parameter Value"], color='#0033ff', alpha=0.9, label='South', s=40, linewidth=0)
+    axs[i].scatter(df_lege_filtered[i]["Time Stamp"].dt.total_seconds() / (24 * 60 * 60), df_lege_filtered[i]["Parameter Value"], color='green', alpha=0.9, label='LEGe', s=40, linewidth=0)
+    
+    # Set the custom y-label instead of the dataset label
+    axs[i].set_ylabel(custom_y_labels[i], fontsize=60, labelpad=30)
+    axs[i].tick_params(axis='x', labelsize=60, length=9, width=3)
+    axs[i].tick_params(axis='y', labelsize=64, length=9, width=3)
+    
+    # Adjust legend position to avoid blocking data
+    if i == 1:
+        legend = axs[i].legend(fontsize=60, loc='best', markerscale=3, frameon=False, labelspacing=0.2, handletextpad=-0.3)
+        for text, color in zip(legend.get_texts(), ['red', '#0033ff', 'green']):
+            text.set_color(color)
+    
+    
+    # Set consistent x-axis limits
+    axs[i].set_xlim([xmin, xmax])
+    
+    # Set y-axis limits for specific plots if necessary
+    if param == "11:PRTD 1":
+        axs[i].set_ylim([-200, 30])
+    elif param == "13:Ambient Temperature":
+        axs[i].set_ylim([10, 50])
+    elif param == "73:DC Detector Leakage Current":
+        axs[i].set_ylim([-70, 300])
+    elif param == "16:Charge Loop DC Level":
+        axs[i].set_ylim([-2, 0.1])
+
+# Only add the x-axis label to the bottom plot
+axs[-1].set_xlabel("Time (days)", fontsize=60, labelpad=20)
+
+
+# Save the figure
+plt.savefig(r'D:\X\out\Bayesian_VS\Fig_PXCT_iPA_log.png')
