efa42c63b38802aae1aba3afd0523f00dcce5cf6
Lijie Sun <47992432+sunlijie-msu@users.noreply.github.com>
2024-12-24 14:38:09 -0500

    update PCGP_numPCs

--- Bayesian_DSL/Bayesian_DSL.py
+++ Bayesian_DSL/Bayesian_DSL.py
@@ -19,7 +19,7 @@ import pylab
 
 
 # Read data from CSV files
-print("[Step 1: Read and check input files.]")
+print("\n[Step 1: Read and check input files.]")
 project_path = os.path.abspath("Bayesian_DSL.py")
 dir_path = os.path.dirname(project_path)
 
@@ -51,7 +51,7 @@ model_y_var_peakrange = model_y_var_fitrange[:,bin_start:bin_stop] # Select mode
 
 
 print("Path: ", dir_path + '\DSL_' + peak + dataset_Tau + '\DSL_' + peak + '_data.csv')
-print("Dimensions of data_fitrange:")
+print("\nDimensions of data_fitrange:")
 print("Shape:", data_fitrange.shape)
 print("Rows:", len(data_fitrange))
 print("Columns:", len(data_fitrange[0]))
@@ -109,9 +109,9 @@ fitrange_min = data_x_values_fitrange[0]
 fitrange_max = data_x_values_fitrange[num_bins_fitrange-1]
 num_model_runs = len(model_parameter_values)  # len(2Darray) will give you the number of rows in the 2D array.
 
-print("peakrange_min: ", peakrange_min,", peakrange_max: ", peakrange_max)
+print("\npeakrange_min: ", peakrange_min,", peakrange_max: ", peakrange_max)
 print("fitrange_min: ", fitrange_min, ", fitrange_max: ", fitrange_max)
-print("num_bins_peakrange: ", num_bins_peak, ",  num_bins_fitrange: ", num_bins_fitrange)
+print("\nnum_bins_peakrange: ", num_bins_peak, ",  num_bins_fitrange: ", num_bins_fitrange)
 
 num_bins_peak = len(model_y_values_peakrange[0])  # len(2Darray[0]) will give you the number of elements in the first row of the 2D array, i.e., the number of columns in the 2D array.
 num_bins_fitrange = len(model_y_values_fitrange[0])
@@ -120,7 +120,7 @@ print("num_bins_peakrange: ", num_bins_peak, ",  num_bins_fitrange: ", num_bins_
 # Select some model runs as training runs. You may select all the runs.
 # rndsample = sample(range(0, 324), 324)  # generates a list of 324 random numbers from the range 0 to 324 and assigns it to the variable rndsample.
 if peak== '23Mg7333':
-    number_of_fullmodel_runs = 190
+    number_of_fullmodel_runs = 201
     ymax = round(max(data_y_values_fitrange) + max(data_y_varhigh_fitrange) * 2.5)
 if peak == '31S1248':
     number_of_fullmodel_runs = 324
@@ -133,21 +133,23 @@ if peak == '31S4156':
     ymax = 30
 
 
-# randomly selects 250 unique integers from this sequence and assigns them to the variable rndsample_train.
-# rndsample_train = sample(range(0, number_of_fullmodel_runs), 149) # range(0,n) means the sequence of random numbers from 0 to n-1. # modify 
-rndsample_train = list(range(0, number_of_fullmodel_runs, 1)) # 1 means select all runs; 2 means select every other run, 0, 2, 4, 6, 8, etc.
-print('Select training runs: ', rndsample_train)
+# randomly selects unique integers from this sequence and assigns them to the variable rndsample_train.
+training_size = int(0.75 * number_of_fullmodel_runs) # Calculate the number of training samples (80% of the total)
+rndsample_train = sample(range(number_of_fullmodel_runs), training_size) # Randomly select indices for the training set
+# rndsample_train = sample(range(0, number_of_fullmodel_runs), 201) # range(0,n) means the sequence of random numbers from 0 to n-1.
+# rndsample_train = list(range(0, number_of_fullmodel_runs, 1)) # 1 means select all runs; 2 means select every other run, 0, 2, 4, 6, 8, etc.
+print('\nSelect training runs: ', rndsample_train)
 model_y_values_peakrange_train = model_y_values_peakrange[rndsample_train, :]  # model_y_values_train is a subset of model_y_values where the rows are selected using the rndsample list and all columns are included by specifying : for the second index.
 model_y_values_fitrange_train = model_y_values_fitrange[rndsample_train, :]  # model_y_values_train is a subset of model_y_values where the rows are selected using the rndsample list and all columns are included by specifying : for the second index. # for visualization purpose only
 model_y_var_peakrange_train = model_y_var_peakrange[rndsample_train, :]
 model_y_var_fitrange_train = model_y_var_fitrange[rndsample_train, :]
 model_parameter_values_train = model_parameter_values[rndsample_train, :]
 
-# Select the rest  model runs as test runs. # modify
-# rndsample_test = [x for x in range(number_of_fullmodel_runs) if x not in rndsample_train]
-rndsample_test = list(range(1, number_of_fullmodel_runs, 2)) # 1 means select all runs; 2 means select every other run, 1, 3, 5, 7, 9, etc.
+# Select the rest model runs as test runs.
+rndsample_test = [x for x in range(number_of_fullmodel_runs) if x not in rndsample_train]
+# rndsample_test = list(range(1, number_of_fullmodel_runs, 3)) # 1 means select all runs; 2 means select every other run, 1, 3, 5, 7, 9, etc.
 
-print('Select test runs: ', rndsample_test)
+print('\nSelect test runs: ', rndsample_test)
 model_y_values_peakrange_test = model_y_values_peakrange[rndsample_test, :]  # model_y_values_train is a subset of model_y_values where the rows are selected using the rndsample list and all columns are included by specifying : for the second index.
 model_y_var_peakrange_test = model_y_var_peakrange[rndsample_test, :]
 model_parameter_values_test = model_parameter_values[rndsample_test, :]
@@ -162,7 +164,7 @@ plt.rcParams['mathtext.fontset'] = 'custom'
 plt.rcParams['mathtext.rm'] = 'Times New Roman'
 plt.rcParams['mathtext.it'] = 'Times New Roman:italic'
 
-print("[Step 2: Plot model training runs (prior) vs data.]")
+print("\n[Step 2: Plot model training runs (prior) vs data.]")
 fig, ax_prior = plt.subplots(figsize=(36, 12))
 fig.subplots_adjust(left=0.08, bottom=0.18, right=0.98, top=0.96)
 
@@ -193,7 +195,7 @@ plt.savefig(peak + dataset_Tau + '_prior.png')
 # plt.show()
 
 
-print("[Step 3: Principal Component Analysis (PCA).]")
+print("\n[Step 3: Principal Component Analysis (PCA).]")
 
 def plot_explained_variance(singular_values): # singular_values is related to the eigenvalues of the covariance matrix.
     # Compute the individual explained variances
@@ -208,7 +210,7 @@ def plot_explained_variance(singular_values): # singular_values is related to th
     
     # Create the plot with the specified size
     fig, ax_variance = plt.subplots(figsize=(36, 12))
-    fig.subplots_adjust(left=0.10, bottom=0.18, right=0.98, top=0.96)
+    fig.subplots_adjust(left=0.09, bottom=0.18, right=0.98, top=0.96)
     
     # Create the bar plot for individual variances
     bar = ax_variance.bar(range(1, len(explained_variances) + 1), explained_variances, alpha=0.5, color='g', label='Individual Explained Variance', width=0.5)
@@ -218,8 +220,8 @@ def plot_explained_variance(singular_values): # singular_values is related to th
 
     # Adding percentage values on top of bars and dots
     for i, (bar, cum_val) in enumerate(zip(bar, cumulative_variances)):
-         ax_variance.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), f'{explained_variances[i]*100:.1f}%', ha='center', va='bottom', fontsize=34)
-         ax_variance.text(i+1, cum_val, f'{cum_val*100:.1f}%', ha='center', va='bottom', fontsize=34)
+         ax_variance.text(bar.get_x() + bar.get_width() / 2, bar.get_height(), f'{explained_variances[i]*100:.1f}%', ha='center', va='bottom', fontsize=32)
+         ax_variance.text(i+1, cum_val, f'{cum_val*100:.1f}%', ha='center', va='bottom', fontsize=32)
 
     # Aesthetics for the plot
     ax_variance.set_xlabel('Principal Components', fontsize=60, labelpad=20)
@@ -229,8 +231,8 @@ def plot_explained_variance(singular_values): # singular_values is related to th
     # ax_variance.yaxis.set_major_formatter(plt.FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
     ax_variance.legend(loc='lower left', bbox_to_anchor=(0.55, 0.25)) # (x, y)
     ax_variance.tick_params(axis='both', which='major', labelsize=60, length=9, width=2)
-    ax_variance.set_xlim(0.3, 20.7)
-    ax_variance.set_ylim(3e-3, 1.9)
+    ax_variance.set_xlim(0.3, 24.7)
+    ax_variance.set_ylim(3e-4, 1.9)
     ax_variance.set_yscale('log')
     ax_variance.grid(True)
     # plt.show()
@@ -238,21 +240,21 @@ def plot_explained_variance(singular_values): # singular_values is related to th
     
 
 # (No filter) Fit an emulator via 'PCGP'
-print("[Step 4: Model emulation.]")
-
-# emulator_1 = emulator(x=data_x_values_peakrange,
-#                       theta=model_parameter_values_train,
-#                       f=model_y_values_peakrange_train.T,
-#                       method='PCGP_numPCs',
-#                       args={'num_pcs': 24})  # Specify the number of principal components
-# C:\Users\sun\AppData\Local\Programs\Python\Python311\Lib\site-packages\surmise\emulationmethods\PCGP_numPCs.py # modify
-# PCGP_numPCs.py is a modified version of PCGP.py that allows the user to specify the number of principal components to be used in the emulator. The number of principal components is specified using the args dictionary with the key 'num_pcs'. If num_pcs is not specified, it falls back to epsilon = 0.01.
+print("\n[Step 4: Model emulation.]")
 
 emulator_1 = emulator(x=data_x_values_peakrange,
                       theta=model_parameter_values_train,
                       f=model_y_values_peakrange_train.T,
-                      method='PCGP',
-                      args={'epsilon': 1e-50}) # Typically, we want to keep the principal components that capture the most variance. The epsilon parameter is used to set a threshold for filtering PCs with explained variances > epsilon.
+                      method='PCGP_numPCs',
+                      args={'num_pcs': 24})  # Specify the number of principal components
+# C:\Users\sun\AppData\Local\Programs\Python\Python311\Lib\site-packages\surmise\emulationmethods\PCGP_numPCs.py # modify
+# PCGP_numPCs.py is a modified version of PCGP.py that allows the user to specify the number of principal components to be used in the emulator. The number of principal components is specified using the args dictionary with the key 'num_pcs'. If num_pcs is not specified, it falls back to epsilon = 0.1.
+
+# emulator_1 = emulator(x=data_x_values_peakrange,
+#                       theta=model_parameter_values_train,
+#                       f=model_y_values_peakrange_train.T,
+#                       method='PCGP',
+#                       args={'epsilon': 1e-50}) # Typically, we want to keep the principal components that capture the most variance. The epsilon parameter is used to set a threshold for filtering PCs with explained variances > epsilon.
 
 # prior_min = [0, 4154, 0.4, 0.4]
 # prior_max = [30, 4158, 2.0, 2.0]
@@ -288,16 +290,21 @@ emulator_1 = emulator(x=data_x_values_peakrange,
 
 
 # after fitting the emulator
-print("Emulation method: ", emulator_1.method)
-if emulator_1.method == 'PCGP_numPCs':
+print(emulator_1._info['param_desc'])
+print("\nEmulation method: ", emulator_1.method)
+print("\nfitinfo keys:", emulator_1._info.keys())
+
+method_str = str(emulator_1.method)
+if 'PCGP_numPCs' in method_str:
     singular_values = emulator_1._info['singular_values']
     plot_explained_variance(singular_values)
 
 
-print("[Step 5: Diagnostics plots.]")
+
+print("\n[Step 5: Diagnostics plots.]")
 pred_emu = emulator_1.predict(data_x_values_peakrange, model_parameter_values_test)  # predict at some parameter values and x values
 pred_m, pred_var = pred_emu.mean(), pred_emu.var()  # Returns the mean and variance at theta and x in the prediction. pred_var represents the diagonal elements of the emulator covariance matrix, namely, the predictive variances.
-print("pred_m:", pred_m.shape, "pred_var:", pred_var.shape)  # pred_m: (59, 162) pred_var: (59, 162)
+print("\npred_m:", pred_m.shape, "pred_var:", pred_var.shape)  # pred_m: (59, 162) pred_var: (59, 162)
 pred_emu_tr = emulator_1.predict(data_x_values_peakrange, model_parameter_values_train)
 pred_m_tr, pred_var_tr = pred_emu_tr.mean(), pred_emu_tr.var()
 print("pred_m_tr:", pred_m_tr.shape, "pred_var_tr:", pred_var_tr.shape)  # pred_m_tr: (59, 324) pred_var_tr: (59, 324)
@@ -312,11 +319,11 @@ axs_emu1[0].scatter(xs, std_err_f_test, alpha=0.5, s=70)
 axs_emu1[0].plot(xs, np.repeat(0, len(xs)), color='red')
 axs_emu1[0].set_xlabel(r'Energy (keV)')
 axs_emu1[0].set_ylabel(r'${(\hat{\mu}_{test} - \mu_{test})}/{\hat{\sigma}_{test}}$')
-axs_emu1[0].set_ylim(-4, 4)
+axs_emu1[0].set_ylim(-5, 5)
 axs_emu1[1].scatter(np.arange(0, n), std_err_nf_test, s=200)
 axs_emu1[1].plot(np.arange(0, n), np.repeat(0, n), color='red')
 axs_emu1[1].set_xlabel(r'Test run')
-axs_emu1[1].set_ylim(-0.3, 0.3)
+axs_emu1[1].set_ylim(-0.6, 0.6)
 plt.savefig(peak + dataset_Tau + '_residual.png')
 #  plt.show()
 
@@ -327,7 +334,7 @@ sse_test = np.sum(errors_test**2)
 rsq_test = 1 - (sse_test / sst_test)
 rsq_test_rounded = np.round(rsq_test, 3)
 
-print(f"Rsq (test) = {rsq_test_rounded}")
+print(f"\nRsq (test) = {rsq_test_rounded}")
 
 # Calculate R^2 for training set
 errors_train = (pred_m_tr - model_y_values_peakrange_train.T).flatten()
@@ -360,13 +367,35 @@ axs_emu2[1].set_title(r'$R^2=$' + str(rsq_train_rounded))
 plt.savefig(peak + dataset_Tau + '_R2.png')
 # plt.show()
 
+mu = 0
+variance = 1
+sigma = np.sqrt(variance)
+x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
+fig, axs_emu3 = plt.subplots(1, 3, figsize=(36, 12))
+fig.subplots_adjust(left=0.07, bottom=0.13, right=0.97, top=0.88)
+axs_emu3[0].hist((pred_m - model_y_values_peakrange_test.T).flatten(), bins=100)
+axs_emu3[0].set_title(r'$\hat{\mu}_{test} - \mu_{test}$', pad=20)
+axs_emu3[0].set_xlim([-1.5, 1.5])
+axs_emu3[1].hist(std_err_f_test, density=True, bins=100)  # standardized error
+axs_emu3[1].plot(x, sps.norm.pdf(x, mu, sigma), color='red')
+axs_emu3[1].set_title(r'${(\hat{\mu}_{test} - \mu_{test})}/{\hat{\sigma}_{test}}$', pad=20)
+axs_emu3[1].set_xlim([-4, 4])
+axs_emu3[2].hist(((pred_m - model_y_values_peakrange_test.T)/model_y_values_peakrange_test.T).flatten(), bins=100)  # relative error
+axs_emu3[2].set_title(r'${(\hat{\mu}_{test} - \mu_{test})}/{\mu_{test}}$', pad=20)
+l = np.arange(-2, 3, 1)/10
+axs_emu3[2].set(xticks=l, xticklabels=l)
+axs_emu3[2].axvline(x=0, ls='--', color='red')
+axs_emu3[2].set_xlim([-0.15, 0.15])
+plt.savefig(peak + dataset_Tau + '_error.png')
+# plt.show()
 
-print('MSE = ', np.round(np.mean(np.sum(np.square(pred_m - model_y_values_peakrange_test.T), axis=1)), 2))  # calculates the mean squared error (MSE), which is another measure of the accuracy of the emulator.
+
+print('\nMSE = ', np.round(np.mean(np.sum(np.square(pred_m - model_y_values_peakrange_test.T), axis=1)), 2))  # calculates the mean squared error (MSE), which is another measure of the accuracy of the emulator.
 print('SSE = ', np.round(np.sum((pred_m - model_y_values_peakrange_test.T)**2), 2))
 
 
 # Define a class for prior of 4 parameters
-print("[Step 6: Prior class specification.]")
+print("\n[Step 6: Prior class specification.]")
 class Prior_DSL23Mg_7333:
     """ This defines the class instance of priors provided to the method. """
     def lpdf(theta):  # log-probability density function of the prior for a given set of parameters theta ['Tau', 'Eg', 'Bkg', 'SP']
@@ -433,8 +462,8 @@ obsvar = (data_y_varlow_peakrange + data_y_varhigh_peakrange)/2
 # print(obsvar)
 
 # Calibrator 1
-print("[Step 7: MCMC sampling.]")
-total_mcmc_samples = 1000
+print("\n[Step 7: MCMC sampling.]")
+total_mcmc_samples = 10000
 if peak == '23Mg7333':
     calibrator_1 = calibrator(emu=emulator_1,
                                            y=data_y_values_peakrange,
@@ -525,7 +554,7 @@ if peak == '31S4156':
             #     print("At sample {}, acceptance rate is {}.".format(i, n_acc/i))
 
 
-print("[Step 8-1: Plot transparent uncertainty band predictions with calibrated parameters.]")
+print("\n[Step 8-1: Plot transparent uncertainty band predictions with calibrated parameters.]")
 
 def plot_pred_interval(calib):
     pred = calib.predict(data_x_values_peakrange)
@@ -538,7 +567,7 @@ def plot_pred_interval(calib):
     posterior_y_upper = np.percentile(rndm_m[:, 0: num_bins_peak], 97.7, axis=0)
     posterior_y_lower = np.percentile(rndm_m[:, 0: num_bins_peak], 2.3, axis=0)
     posterior_y_median = np.percentile(rndm_m[:, 0: num_bins_peak], 50, axis=0)
-    print("posterior_prediction_y_median: ", posterior_y_median)
+    print("\nposterior_prediction_y_median: ", posterior_y_median)
     
     # p2 = ax_post_predict.fill_between(data_x_values_fitrange, smoothed_model_lowcount, smoothed_model_highcount,
     #                  color='red', alpha=0.3, linewidth=0, zorder=1)
@@ -586,7 +615,7 @@ def plot_pred_interval(calib):
 
 plot_pred_interval(calibrator_1)
 
-print("[Step 8-2: Plot posterior samples.]")
+print("\n[Step 8-2: Plot posterior samples.]")
 def plot_theta(calib, whichtheta):
     fig, axs_trace = plt.subplots(3, 1, figsize=(30, 30))
     fig.subplots_adjust(left=0.10, bottom=0.07, right=0.96, top=0.97, hspace=0.3)
@@ -638,7 +667,7 @@ def plot_theta(calib, whichtheta):
 plot_theta(calibrator_1, 0)
 
 
-print("[Step 8-3: Plot 2D posterior distributions of parameters.]")
+print("\n[Step 8-3: Plot 2D posterior distributions of parameters.]")
 if peak == '23Mg7333':
     theta_prior = Prior_DSL23Mg_7333.rnd(total_mcmc_samples)  # draw 1000 random parameters from the prior
     theta_post = calibrator_1.theta.rnd(total_mcmc_samples)
@@ -663,8 +692,10 @@ if peak == '31S4156':
     dfpost = pd.DataFrame(theta_post, columns=['Lifetime', '$\gamma$-ray Energy', 'Background', 'Stopping Power'])
     dfprior = pd.DataFrame(theta_prior, columns=['Lifetime', '$\gamma$-ray Energy', 'Background', 'Stopping Power'])
 
-print("One prior sample: ", theta_prior[0])
+np.set_printoptions(precision=3, suppress=True) # Set the precision to 3 decimal places and suppress scientific notation
+print("One prior sample:", theta_prior[0])
 print("One posterior sample:", theta_post[0])
+
 
 #  a function from the Pandas library that is used to create a dataframe object. Dataframe is a two-dimensional, size-mutable, and tabular data structure with columns of potentially different types.
 
@@ -759,4 +790,4 @@ plt.savefig(peak + dataset_Tau + '_posterior.png')
 # plt.show()
     
 
-print("[The End]")
+print("\n[The End]")
